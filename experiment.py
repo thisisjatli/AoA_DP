2ebb748403f40307f849cfcff6fcc73d8932f859
Jat Li <janet881005@gmail.com>
2023-11-08 14:33:26 -0500

    refactor: remove redundant comments

--- experiment.py
+++ experiment.py
@@ -14,16 +14,8 @@ from algo4 import algo4
 from algorithm import alg5, alg6a, alg6b, alg7, alg8
 
 def generate_input(n, cost):
-    # k < n
-    # ceil(n/k) <= m <= n
     added_len = abs(n-len(cost))
     rd_cost = np.random.randint(0, 30, size=added_len, dtype='int')
-    # k = random.randint(1, min(n+1, 5))
-    # m = random.randint(math.ceil(n/k), min(math.ceil(n/k)+5, n))
-    # print('n:', n)
-    # print('k:', k)
-    # print('m:', m)
-    # print('Cost:', cost)
     cost = cost + [int(rd_cost[i]) for i in range(rd_cost.size)]   # in order to store in json file
     return cost
 
@@ -66,63 +58,7 @@ def runtime_test(n, k, m, cost, algo='1'):
     total_time = time.time() - start_time
     return total_time
 
-# def runtime_test_v2(n, k, m, cost):
-#     runtime_results = {}
-
-#     start_time = time.time()
-#     algo1(n, k, cost)
-#     print("\nalgo1 done")
-#     runtime_results['1'] = time.time() - start_time
-    
-#     start_time = time.time()
-#     algo2A(n, k, cost)
-#     print("\nalgo2A done")
-#     runtime_results['2A'] = time.time() - start_time
-    
-#     start_time = time.time()
-#     bottom_up(n, k, cost)
-#     print("\nalgo2B done")
-#     runtime_results['2B'] = time.time() - start_time
-    
-#     start_time = time.time()
-#     algo3(n, k, cost)
-#     print("\nalgo3 done")
-#     runtime_results['3'] = time.time() - start_time
-    
-#     start_time = time.time()
-#     algo4(n, k, cost)
-#     print("\nalgo4 done")
-#     runtime_results['4'] = time.time() - start_time
-    
-#     start_time = time.time()
-#     alg5(n, k, m, cost)
-#     print("algo5 done")
-#     runtime_results['5'] = time.time() - start_time
-    
-#     start_time = time.time()
-#     alg6a(n, k, m, cost)
-#     print("algo6A done")
-#     runtime_results['6A'] = time.time() - start_time
-    
-#     start_time = time.time()
-#     alg6b(n, k, m, cost)
-#     print("algo6B done")
-#     runtime_results['6B'] = time.time() - start_time
-    
-#     start_time = time.time()
-#     alg7(n, k, m, cost)
-#     print("algo7 done")
-#     runtime_results['7'] = time.time() - start_time
-    
-#     start_time = time.time()
-#     alg8(n, k, m, cost)
-#     print("algo8 done")
-#     runtime_results['8'] = time.time() - start_time
-    
-#     return runtime_results
-
 def experiment_v1(n, k, m, cost, algo_list):
-    # algo_list = ['1', '2A', '2B', '3', '4', '5', '6A', '6B', '7', '8']
     with Pool() as pool:
         async_results = [pool.apply_async(runtime_test, (n, k, m, cost, algo,)) for algo in algo_list]
         results = [ar.get() for ar in async_results]
@@ -131,37 +67,17 @@ def experiment_v1(n, k, m, cost, algo_list):
     print(results)
     return results
 
-# def experiment_v2(n):
-#     cost, k, m = generate_input(n)
-#     results = runtime_test_v2(n, k, m, list(cost))
-#     print(results)
-
 if __name__ == "__main__":
-    # n_list = [10, 50, 100, 500, 1000, 5000, 10000, 50000, 99999]
-    # n_list = [100, 500, 1000, 5000, 10000, 20000, 30000, 40000]
-    # n_list = [100, 500, 1000, 5000, 10000, 15000, 20000, 25000, 30000, 35000, 40000]
-    n_list = [100, 500, 1000, 2000, 3000, 4000, 5000]
-    # n_list = [10000, 20000, 30000, 40000]
-    # n_list = [10, 15, 20, 25, 30, 35]    # for brute-force, the size limit is around 60
-    # n_list = [20, 30, 40, 50, 60]
-    # k, m = 80, 60
+    n_list = [100, 500, 1000, 5000, 10000, 15000, 20000, 25000, 30000, 35000, 40000]
     k, m = 100, 60
-    # k, m = 1000, 50
-    # k, m = 5, 8
-    # k, m = 500, 200 # experimental purpose
-    # algo_list = ['2A', '2B', '3', '4', '6A', '6B', '7', '8']
-    # algo_list = ['2A', '2B', '3', '4']
-    algo_list = ['6A', '6B', '7', '8']
-    # algo_list = ['2A', '2B']
+    algo_list = ['1', '2A', '2B', '3', '4', '5', '6A', '6B', '7', '8']
 
     stats = {n: {} for n in n_list}
     cost = []
 
     for n in n_list:
         cost = generate_input(n, cost)
-        # stats[n]["cost"] = cost
         print("n:", n)
-        # print("Cost:", cost)
         results = experiment_v1(n, k, m, cost, algo_list)
         stats[n]["runtime"] = {algo: r for algo, r in zip(algo_list, results)}
 
--- plot.py
+++ plot.py
@@ -9,19 +9,13 @@ with open('experimental_results/p2_without_bf_small.json', 'r') as fr:
     data = json.load(fr)
 
 n_list = list(data.keys())
-# results = []
-# for d in data.values():
-#     results = results + [d['runtime'][0]]
-# print(results)
-# algo_list = ['2A', '2B', '3', '4']
-algo_list = ['6A', '6B', '7', '8']
-# algo_list = ['5']
+
+algo_list = ['1', '2A', '2B', '3', '4', '5', '6A', '6B', '7', '8']
 p_list = {'1': 1, '2A': 1, '2B': 1, '3': 1, '4': 1, '5': 2, '6A': 2, '6B': 2, '7': 2, '8': 2}
 
 data_list = [[algo, int(n), data[n]["runtime"][algo], p_list[algo]] for n in n_list for algo in algo_list]
 
 df = pd.DataFrame(data_list, columns=['Algo', 'n', 'Runtime', 'Problem'])
-print(df)
 
 sns.lineplot(df.query("Problem == 2"), x='n', y='Runtime', hue='Algo', markers=True, style='Algo').set_title("Problem 2")
 plt.show()
